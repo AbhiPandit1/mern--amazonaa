878d0f86e4d3ac0c80cca88b988c6374bd08e5be
abhishek <224abhishekjha@gmail.com>
2023-07-12 05:21:04 +0100

    first-02

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+
+
+function App() {
+  return (
+    <div >
+      <header >
+        <a href="/">amazona</a>
+      </header>
+      <main>list Product</main>
+    </div>
+
+  );
+}
+
+export default App;
