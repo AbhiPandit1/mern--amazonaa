c5049acfbe8511a46ccc1c7c8007542527a19141
abhishek <224abhishekjha@gmail.com>
2023-07-13 02:40:02 +0100

    Till api fetch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,40 +1,34 @@
-import data from "./data";
+
+import {BrowserRouter,Link,Route,Routes} from 'react-router-dom';
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from './screens/ProductScreen';
 
 
 function App() {
+
   return (
-    <div >
+    <BrowserRouter>
+    <div>
       <header >
-        <a href="/">amazona</a>
+        <Link to="/">amazona</Link>
       </header>
       <main>
-      <h1> Featured Products</h1>
-      <div className="products">
-      {
-        data.products.map((product)=>(
-          
-          <div className="product" key={product.slug}>
-
-          <a href={`/product/product/${product.slug}`}>
-          <img src={product.image} alt ={product.name}/>
-          </a>
-
-          <div className='product-info'>
-          <a href={`/product/product/${product.slug}`}>
-          <p>{product.name}</p>
-          </a>
-          <p><strong>${product.price}</strong></p>
-          <button>Add to Cart</button>
-       </div>
-       </div>
-
-        ))
-      }
-      </div>
+      <Routes>
+         <Route path='/product/:slug' element={<ProductScreen/>}/>
+        <Route path='/' element={<HomeScreen/>}/>
+      </Routes>
       </main>
-    </div>
+      </div>
 
-  );
-}
+      </BrowserRouter>
+     
+        )
+      }
+      
+      
+    
+   
+
+  
 
 export default App;
