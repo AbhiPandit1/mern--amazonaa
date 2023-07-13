b4ec380e4ac12e374e101b523844df21411259e4
abhishek <224abhishekjha@gmail.com>
2023-07-13 02:47:18 +0100

    This all

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,34 +1,76 @@
-import { Link } from "react-router-dom"
-import data from "../data"
+import { useEffect, useReducer } from 'react';
+import { Link } from 'react-router-dom';
+import logger from 'use-reducer-logger'
 
+import axios from 'axios';
+// import data from '../data';
 
+const reducer=(state,action)=>{
+  switch(action.type){
+    case 'FETCH_REQUEST':
+      return{...state,loading:true}
+    case 'FETCH_SUCCESS':
+      return{...state,products:action.payload, loading:false}
+    
+    case 'FETCH_FAIL':
+      return {...state,loading:false,error:action.payload};
+    default:
+      return state;
 
-function HomeScreen(){
-    return (
+  }
+}
+
+function HomeScreen() {
+  //const [products, setProducts] = useState([]);
+  const[{loading,error,products},dispatch]=useReducer(logger(reducer),{
+    products:[],
+    loading:true,error:'',
+  })
+  useEffect(() => {
+    const fetchData = async () => {
+      dispatch({type:'FETCH_REQUEST'})
+      try{
+        const result = await axios.get('/api/products');
+        dispatch({type:'FETCH_SUCCESS',payload:result.data})
+}
+      catch(err){
+        dispatch({type:'FETCH_FAIL',payload:err.message})
+      }
+      
+      //setProducts(result.data);
+    };
+    fetchData();
+  }, []);
+  return (
     <div>
-    <h1> Featured Products</h1>
-    <div className="products">
-    {
-      data.products.map((product)=>(
+      <h1>Featured Products</h1>
+      <div className="products">
+      {
+        loading?(<div>Loading...</div>
+        ):error?(
+          <div>{error}</div>
+        ):
+      (
         
-        <div className="product" key={product.slug}>
-
-        <Link to={`/product/product/${product.slug}`}>
-        <img src={product.image} alt ={product.name}/>
-        </Link>
-
-        <div className='product-info'>
-        <Link to={`/product/product/${product.slug}`}>
-        <p>{product.name}</p>
-        </Link>
-        <p><strong>${product.price}</strong></p>
-        <button>Add to Cart</button>
-     </div>
-     </div>
-      ))}
-
+        products.map((product) => (
+          <div className="product" key={product.slug}>
+            <Link to={`/product/${product.slug}`}>
+              <img src={product.image} alt={product.name} />
+            </Link>
+            <div className="product-info">
+              <Link to={`/product/${product.slug}`}>
+                <p>{product.name}</p>
+              </Link>
+              <p>
+                <strong>${product.price}</strong>
+              </p>
+              <button>Add to cart</button>
+            </div>
+          </div>
+        ))
+        )}
       </div>
     </div>
-)}
-
-export default HomeScreen
+  );
+        }
+export default HomeScreen;
