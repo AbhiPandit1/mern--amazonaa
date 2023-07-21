7262b7203f9ef0760e58d7c2ac832eb7e9b4af9a
Abhishek <224abhishekjha@gmail.com>
2023-07-21 21:58:51 +0100

    Before Connecting Database

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
+import { HelmetProvider } from 'react-helmet-async';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 import App from './App';
+import { StoreProvider } from './Store';
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <StoreProvider>
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
+    </StoreProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
