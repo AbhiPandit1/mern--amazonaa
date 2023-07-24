f18154f90283838e4ac590a6246aee37be8be04a
Abhishek <224abhishekjha@gmail.com>
2023-07-24 14:24:12 +0100

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 import express from 'express';
- import data from './data.js';
+ import path from 'path';
  import mongoose from 'mongoose';
  import dotenv from 'dotenv';
  import seedRouter from './routes/seedRoutes.js';
@@ -30,6 +30,11 @@ mongoose
  app.use('/api/products', productRouter);
  app.use('/api/users',userRouter)
  app.use('/api/orders', orderRouter);
+
+
+ const __dirname=path.resolve()
+ app.use(express.static(path.join(__dirname,'/fronted/build')));
+ app.get('*',(req,res)=> res.sendFile(path.join(__dirname,'/frontend/build/index.html')))
 
 
  //errorHandler
